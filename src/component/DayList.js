f566e8dd9f29b7e7fdbe5ef2e6c25aeabdf33831
jhy3990585 <jhy3990585@innogrid.com>
2022-06-14 15:01:21 +0900

    feat: json-server 사용해보기

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,14 +1,26 @@
-import dummy from "../db/data.json";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 export default function DayList() {
+    const [days, setDays] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3002/days')
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                setDays(data);
+            });
+    }, []);
+
     return (
         <ul className="list_day">
-        {dummy.days.map(day => (
-            <li key={day.id}>
-                <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-            </li>
-        ))}
-    </ul>
+            {days.map(day => (
+                <li key={day.id}>
+                    <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+                </li>
+            ))}
+        </ul>
     );
 }
